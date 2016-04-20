d5ca77e8bdfb1d48faaf62c5ef0e6b9dfa827cae
adityaKant <adikant9@gmail.com>
2016-04-21 01:04:36 +0530

    header transition.

--- app/assets/javascripts/header_transition.js
+++ app/assets/javascripts/header_transition.js
@@ -1,3 +1,12 @@
-$(document).on("page:change",function(){
-  alert("asdasd");
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 265){
+      $('#header').addClass("header-trans")
+    }
+    if($(window).scrollTop() < 265){
+      $('#header').removeClass("header-trans")
+    }
+  });
+
 });
